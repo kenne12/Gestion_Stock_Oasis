2c3dceb0f786df673a2be249b68139a5a7664905
Kenne Gervais <kenne_gervais@yahoo.fr>
2021-02-25 09:56:28 +0100

    Nettoyage du controller : Famille Produit

--- Gestion_Stock_Oasis-war/src/java/controllers/famille/AbstractFamilleController.java
+++ Gestion_Stock_Oasis-war/src/java/controllers/famille/AbstractFamilleController.java
@@ -1,97 +1,91 @@
-/*    */ package controllers.famille;
-/*    */ 
-/*    */ import entities.Famille;
-/*    */ import java.util.ArrayList;
-/*    */ import java.util.List;
-/*    */ import javax.ejb.EJB;
-/*    */ import sessions.FamilleFacadeLocal;
-/*    */ import sessions.MouchardFacadeLocal;
-/*    */ import utils.Routine;
-/*    */ 
-/*    */ public class AbstractFamilleController
-/*    */ {
-/*    */ 
-/*    */   @EJB
-/*    */   protected FamilleFacadeLocal familleFacadeLocal;
-/*    */   protected Famille famille;
-/* 25 */   protected List<Famille> familles = new ArrayList();
-/*    */ 
-/*    */   @EJB
-/*    */   protected MouchardFacadeLocal mouchardFacadeLocal;
-/* 30 */   protected Boolean detail = Boolean.valueOf(true);
-/* 31 */   protected Boolean modifier = Boolean.valueOf(true);
-/* 32 */   protected Boolean consulter = Boolean.valueOf(true);
-/* 33 */   protected Boolean imprimer = Boolean.valueOf(true);
-/* 34 */   protected Boolean supprimer = Boolean.valueOf(true);
-/*    */ 
-/* 36 */   protected Routine routine = new Routine();
-/*    */ 
-/* 38 */   protected String mode = "";
-/*    */ 
-/*    */   public Boolean getDetail() {
-/* 41 */     return this.detail;
-/*    */   }
-/*    */ 
-/*    */   public void setDetail(Boolean detail) {
-/* 45 */     this.detail = detail;
-/*    */   }
-/*    */ 
-/*    */   public Boolean getModifier() {
-/* 49 */     return this.modifier;
-/*    */   }
-/*    */ 
-/*    */   public void setModifier(Boolean modifier) {
-/* 53 */     this.modifier = modifier;
-/*    */   }
-/*    */ 
-/*    */   public Boolean getConsulter() {
-/* 57 */     return this.consulter;
-/*    */   }
-/*    */ 
-/*    */   public void setConsulter(Boolean consulter) {
-/* 61 */     this.consulter = consulter;
-/*    */   }
-/*    */ 
-/*    */   public Boolean getImprimer() {
-/* 65 */     return this.imprimer;
-/*    */   }
-/*    */ 
-/*    */   public void setImprimer(Boolean imprimer) {
-/* 69 */     this.imprimer = imprimer;
-/*    */   }
-/*    */ 
-/*    */   public Boolean getSupprimer() {
-/* 73 */     return this.supprimer;
-/*    */   }
-/*    */ 
-/*    */   public void setSupprimer(Boolean supprimer) {
-/* 77 */     this.supprimer = supprimer;
-/*    */   }
-/*    */ 
-/*    */   public Famille getFamille() {
-/* 81 */     return this.famille;
-/*    */   }
-/*    */ 
-/*    */   public void setFamille(Famille famille) {
-/* 85 */     this.modifier = (this.supprimer = this.detail = Boolean.valueOf(famille == null));
-/* 86 */     this.famille = famille;
-/*    */   }
-/*    */ 
-/*    */   public List<Famille> getFamilles() {
-/* 90 */     this.familles = this.familleFacadeLocal.findAllRange();
-/* 91 */     return this.familles;
-/*    */   }
-/*    */ 
-/*    */   public void setFamilles(List<Famille> familles) {
-/* 95 */     this.familles = familles;
-/*    */   }
-/*    */ 
-/*    */   public Routine getRoutine() {
-/* 99 */     return this.routine;
-/*    */   }
-/*    */ }
+package controllers.famille;
 
-/* Location:           I:\GESTION_STOCK\GESTION_STOCK-war_war\WEB-INF\classes\
- * Qualified Name:     controllers.famille.AbstractFamilleController
- * JD-Core Version:    0.6.2
- */
+import entities.Famille;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.EJB;
+import sessions.FamilleFacadeLocal;
+import sessions.MouchardFacadeLocal;
+import utils.Routine;
+
+public class AbstractFamilleController {
+
+    @EJB
+    protected FamilleFacadeLocal familleFacadeLocal;
+    protected Famille famille;
+    protected List<Famille> familles = new ArrayList();
+
+    @EJB
+    protected MouchardFacadeLocal mouchardFacadeLocal;
+    protected Boolean detail = true;
+    protected Boolean modifier = true;
+    protected Boolean consulter = true;
+    protected Boolean imprimer = true;
+    protected Boolean supprimer = true;
+
+    protected Routine routine = new Routine();
+
+    protected String mode = "";
+
+    public Boolean getDetail() {
+        return this.detail;
+    }
+
+    public void setDetail(Boolean detail) {
+        this.detail = detail;
+    }
+
+    public Boolean getModifier() {
+        return this.modifier;
+    }
+
+    public void setModifier(Boolean modifier) {
+        this.modifier = modifier;
+    }
+
+    public Boolean getConsulter() {
+        return this.consulter;
+    }
+
+    public void setConsulter(Boolean consulter) {
+        this.consulter = consulter;
+    }
+
+    public Boolean getImprimer() {
+        return this.imprimer;
+    }
+
+    public void setImprimer(Boolean imprimer) {
+        this.imprimer = imprimer;
+    }
+
+    public Boolean getSupprimer() {
+        return this.supprimer;
+    }
+
+    public void setSupprimer(Boolean supprimer) {
+        this.supprimer = supprimer;
+    }
+
+    public Famille getFamille() {
+        return this.famille;
+    }
+
+    public void setFamille(Famille famille) {
+        this.modifier = (this.supprimer = this.detail = (famille == null));
+        this.famille = famille;
+    }
+
+    public List<Famille> getFamilles() {
+        this.familles = this.familleFacadeLocal.findAllRange();
+        return this.familles;
+    }
+
+    public void setFamilles(List<Famille> familles) {
+        this.familles = familles;
+    }
+
+    public Routine getRoutine() {
+        return this.routine;
+    }
+}
--- Gestion_Stock_Oasis-war/src/java/controllers/famille/FamilleController.java
+++ Gestion_Stock_Oasis-war/src/java/controllers/famille/FamilleController.java
@@ -12,96 +12,95 @@ import utils.Utilitaires;
 
 @ManagedBean
 @ViewScoped
-public class FamilleController extends AbstractFamilleController
-        implements Serializable {
+public class FamilleController extends AbstractFamilleController implements Serializable {
 
     public void prepareCreate() {
         try {
-            /*  35 */ if (!Utilitaires.isAccess(Long.valueOf(10L))) {
-                /*  36 */ signalError("acces_refuse");
-                /*  37 */ return;
+            if (!Utilitaires.isAccess(10L)) {
+                signalError("acces_refuse");
+                return;
             }
 
-            /*  40 */ this.famille = new Famille();
-            /*  41 */ this.mode = "Create";
-            /*  42 */ RequestContext.getCurrentInstance().execute("PF('FamilleCreerDialog').show()");
+            this.famille = new Famille();
+            this.mode = "Create";
+            RequestContext.getCurrentInstance().execute("PF('FamilleCreerDialog').show()");
         } catch (Exception e) {
-            /*  44 */ signalException(e);
+            signalException(e);
         }
     }
 
     public void prepareEdit() {
         try {
-            /*  50 */ if (!Utilitaires.isAccess(Long.valueOf(11L))) {
-                /*  51 */ signalError("acces_refuse");
-                /*  52 */ return;
+            if (!Utilitaires.isAccess(11L)) {
+                signalError("acces_refuse");
+                return;
             }
-            /*  54 */ this.mode = "Edit";
-            /*  55 */ RequestContext.getCurrentInstance().execute("PF('FamilleCreerDialog').show()");
+            this.mode = "Edit";
+            RequestContext.getCurrentInstance().execute("PF('FamilleCreerDialog').show()");
         } catch (Exception e) {
-            /*  57 */ signalException(e);
+            signalException(e);
         }
     }
 
     public void create() {
         try {
-            /*  63 */ if (this.mode.equals("Create")) {
-                /*  64 */ this.famille.setIdfamille(this.familleFacadeLocal.nextVal());
-                /*  65 */ this.famille.setEtat("True");
-                /*  66 */ this.famille.setDateEnregistre(new Date());
-                /*  67 */ this.famille.setDerniereModif(new Date());
-                /*  68 */ this.familleFacadeLocal.create(this.famille);
-                /*  69 */ Utilitaires.saveOperation(this.mouchardFacadeLocal, "Enregistrement du la famille des produits : " + this.famille.getNom(), SessionMBean.getUserAccount());
-                /*  70 */ this.famille = null;
-                /*  71 */ signalSuccess();
-            } /*  73 */ else if (this.famille != null) {
-                /*  74 */ this.familleFacadeLocal.edit(this.famille);
-                /*  75 */ this.famille = null;
-                /*  76 */ signalSuccess();
+            if (this.mode.equals("Create")) {
+                this.famille.setIdfamille(this.familleFacadeLocal.nextVal());
+                this.famille.setEtat("True");
+                this.famille.setDateEnregistre(new Date());
+                this.famille.setDerniereModif(new Date());
+                this.familleFacadeLocal.create(this.famille);
+                Utilitaires.saveOperation(this.mouchardFacadeLocal, "Enregistrement du la famille des produits : " + this.famille.getNom(), SessionMBean.getUserAccount());
+                this.famille = null;
+                signalSuccess();
+            } else if (this.famille != null) {
+                this.familleFacadeLocal.edit(this.famille);
+                this.famille = null;
+                signalSuccess();
             } else {
-                /*  78 */ JsfUtil.addErrorMessage("Aucun famille selectionné");
+                JsfUtil.addErrorMessage("Aucun famille selectionné");
             }
         } catch (Exception e) {
-            /*  82 */ signalException(e);
+            signalException(e);
         }
     }
 
     public void delete() {
         try {
-            /*  88 */ if (this.famille != null) {
-                /*  90 */ if (!Utilitaires.isAccess(Long.valueOf(12L))) {
-                    /*  91 */ signalError("acces_refuse");
-                    /*  92 */ return;
+            if (this.famille != null) {
+                if (!Utilitaires.isAccess(12L)) {
+                    signalError("acces_refuse");
+                    return;
                 }
 
-                /*  95 */ this.familleFacadeLocal.remove(this.famille);
+                this.familleFacadeLocal.remove(this.famille);
 
-                /*  97 */ Utilitaires.saveOperation(this.mouchardFacadeLocal, "Suppresion de la famille de produit : " + this.famille.getNom(), SessionMBean.getUserAccount());
-                /*  98 */ this.famille = null;
-                /*  99 */ signalSuccess();
+                Utilitaires.saveOperation(this.mouchardFacadeLocal, "Suppresion de la famille de produit : " + this.famille.getNom(), SessionMBean.getUserAccount());
+                this.famille = null;
+                signalSuccess();
             } else {
-                /* 101 */ JsfUtil.addErrorMessage("Aucune Famille selectionnée");
+                JsfUtil.addErrorMessage("Aucune Famille selectionnée");
             }
         } catch (Exception e) {
-            /* 104 */ signalException(e);
+            signalException(e);
         }
     }
 
     public void signalError(String chaine) {
-        /* 109 */ RequestContext.getCurrentInstance().execute("PF('AjaxNotifyDialog').hide()");
-        /* 110 */ this.routine.feedBack("information", "/resources/tool_images/warning.jpeg", this.routine.localizeMessage(chaine));
-        /* 111 */ RequestContext.getCurrentInstance().execute("PF('NotifyDialog1').show()");
+        RequestContext.getCurrentInstance().execute("PF('AjaxNotifyDialog').hide()");
+        this.routine.feedBack("information", "/resources/tool_images/warning.jpeg", this.routine.localizeMessage(chaine));
+        RequestContext.getCurrentInstance().execute("PF('NotifyDialog1').show()");
     }
 
     public void signalSuccess() {
-        /* 115 */ RequestContext.getCurrentInstance().execute("PF('AjaxNotifyDialog').hide()");
-        /* 116 */ this.routine.feedBack("information", "/resources/tool_images/success.png", this.routine.localizeMessage("operation_reussie"));
-        /* 117 */ RequestContext.getCurrentInstance().execute("PF('NotifyDialog1').show()");
+        RequestContext.getCurrentInstance().execute("PF('AjaxNotifyDialog').hide()");
+        this.routine.feedBack("information", "/resources/tool_images/success.png", this.routine.localizeMessage("operation_reussie"));
+        RequestContext.getCurrentInstance().execute("PF('NotifyDialog1').show()");
     }
 
     public void signalException(Exception e) {
-        /* 121 */ RequestContext.getCurrentInstance().execute("PF('AjaxNotifyDialog').hide()");
-        /* 122 */ this.routine.catchException(e, this.routine.localizeMessage("erreur_execution"));
-        /* 123 */ RequestContext.getCurrentInstance().execute("PF('NotifyDialog1').show()");
+        RequestContext.getCurrentInstance().execute("PF('AjaxNotifyDialog').hide()");
+        this.routine.catchException(e, this.routine.localizeMessage("erreur_execution"));
+        RequestContext.getCurrentInstance().execute("PF('NotifyDialog1').show()");
     }
 }
